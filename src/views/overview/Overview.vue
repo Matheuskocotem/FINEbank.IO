4c394cf9bc7d3ff5d17350054c0de920c8091e55
Matheuskocotem <kocotemmatheus@gmail.com>
2025-01-07 02:13:12 -0300

    Overview view

---
+++ src/views/overview/Overview.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <Sidebar />
+    </div>
+
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import Sidebar from '@/components/Sidebar.vue';
+
+export default defineComponent({
+  name: 'YourViewName', 
+  components: {
+    Sidebar, 
+  },
+});
+</script>
+
+
+<style>
+
+</style>
